0923d5d9a7faaa90f85099f6d4ffb9cac52145aa
tehilbi <btehila@cambium.co.il>
2022-10-03 14:59:02 +0300

    Component Updates In Action

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button';
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
-  );
+    const [showParagraph, setShowParagraph] = useState();
+
+    console.log('APP RUNNING!');
+
+    const toggleParagraphHandler = () => {
+        setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    };
+
+    return (
+        <div className='app'>
+            <h1>Hi there!</h1>
+            {showParagraph && <p>This is new!</p>}
+            <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
+        </div>
+    );
 }
 
 export default App;
